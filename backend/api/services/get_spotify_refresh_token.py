690632d2b2f4fc673643dca745e7d6ac25997b80
Pawel Lamza <pawel.lamza@swmansion.com>
2026-07-17 23:41:20 +0200

    added script for getting refresh token; improved spotify client, first fully working version

---
+++ backend/api/services/get_spotify_refresh_token.py
@@ -0,0 +1,102 @@
+from dataclasses import dataclass
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import os
+import secrets
+from pathlib import Path
+import base64
+from urllib.parse import parse_qs, urlparse
+import webbrowser
+
+import requests
+from dotenv import load_dotenv
+
+
+PROJECT_ROOT = Path(__file__).resolve().parents[2]
+load_dotenv(PROJECT_ROOT / ".env")
+
+BASE_AUTH_URL = "https://accounts.spotify.com/api/token"
+BASE_API_URL = "https://api.spotify.com/v1"
+
+captured = {}
+
+class Handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        query = parse_qs(urlparse(self.path).query)
+        captured["code"] = query.get("code", [None])[0]
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
+        self.wfile.write(b"<h1>You can close this tab.</h1>")
+
+    def log_message(self, format, *args):
+        pass  # silence default logging
+
+@dataclass
+class AuthData:
+    client_id: str
+    encoded_client_details: bytes
+    redirect_uri: str
+
+def get_spotify_refresh_token():
+    auth_data = _get_auth_data_from_settings()
+    auth_url = _get_request_user_authorization_url(auth_data)
+
+    print("Opening browser for authorization...")
+    webbrowser.open(auth_url)
+    server = HTTPServer(("localhost", 3000), Handler)
+    server.handle_request()
+
+    return _make_request(captured["code"], auth_data)
+
+def _get_auth_data_from_settings() -> AuthData:
+    client_id = os.getenv("SPOTIFY_CLIENT_ID")
+    client_secret = os.getenv("SPOTIFY_CLIENT_SECRET")
+    redirect_uri = os.getenv("REDIRECT_URI")
+
+    if not client_id or not client_secret or not redirect_uri:
+        raise ValueError("Please set SPOTIFY_CLIENT_ID, SPOTIFY_CLIENT_SECRET and REDIRECT_URI in your /.env file.")
+
+    encoded_client_details = f"{client_id}:{client_secret}".encode("utf-8")
+    return AuthData(
+        client_id=client_id,
+        encoded_client_details=encoded_client_details,
+        redirect_uri=redirect_uri
+    )
+
+def _get_request_user_authorization_url(auth_data: AuthData) -> str:
+    state = secrets.token_urlsafe(16)
+    
+    auth_url = (
+        f"https://accounts.spotify.com/authorize"
+        f"?response_type=code"
+        f"&client_id={auth_data.client_id}"
+        f"&scope=playlist-read-private playlist-read-collaborative"
+        f"&redirect_uri={auth_data.redirect_uri}"
+        f"&state={state}"
+    )
+    
+    return auth_url
+
+def _make_request(code: str, auth_data: AuthData) -> str:
+    try:
+        response = requests.post(
+            BASE_AUTH_URL,
+            data={
+                "grant_type": "authorization_code",
+                "code": code,
+                "redirect_uri": auth_data.redirect_uri
+            },
+            headers={
+                "content-type": "application/x-www-form-urlencoded",
+                "Authorization": "Basic " + base64.b64encode(auth_data.encoded_client_details).decode()
+            }
+        )
+        response.raise_for_status()
+        data = response.json()
+        return data.get("refresh_token")
+    except requests.exceptions.RequestException as e:
+        raise Exception(f"Failed to request access token: {str(e)}")
+
+if __name__ == "__main__":
+    refresh_token = get_spotify_refresh_token()
+    print(refresh_token)
---
+++ backend/api/services/spotify_client.py
@@ -0,0 +1,129 @@
+from dataclasses import dataclass
+import os
+import time
+from pathlib import Path
+import base64
+
+import requests
+from dotenv import load_dotenv
+
+
+PROJECT_ROOT = Path(__file__).resolve().parents[2]
+load_dotenv(PROJECT_ROOT / ".env")
+
+
+_token_cache = {
+    "token": None,
+    "expires_at": 0
+}
+
+class SpotifyClient:
+    """Service for interacting with Spotify API"""
+    def __init__(self) -> None:
+        client_id = os.getenv("SPOTIFY_CLIENT_ID")
+        client_secret = os.getenv("SPOTIFY_CLIENT_SECRET")
+        refresh_token = os.getenv("SPOTIFY_REFRESH_TOKEN")
+        
+        if not client_id or not client_secret:
+            raise ValueError("Please set SPOTIFY_CLIENT_ID and SPOTIFY_CLIENT_SECRET in your /.env file.")
+        if not refresh_token:
+            raise ValueError("SPOTIFY_REFRESH_TOKEN is not set. Run `get_spotify_refresh_token` to get new refresh token.")
+        
+        self.CLIENT_CREDENTIALS = f"{client_id}:{client_secret}".encode("utf-8")
+        self.REFRESH_TOKEN = refresh_token
+
+        self.BASE_AUTH_URL = "https://accounts.spotify.com/api/token"
+        self.BASE_API_URL = "https://api.spotify.com/v1"
+    
+    def fetch_playlist_items(self, playlist_id: str) -> dict:
+        try:
+            headers = self._get_default_headers()
+            response = requests.get(f"{self.BASE_API_URL}/playlists/{playlist_id}/items", headers=headers)
+            response.raise_for_status()
+            return response.json()
+        except requests.exceptions.RequestException as e:
+            raise Exception(f"Failed to fetch playlist data: {str(e)}")
+        
+    def _get_default_headers(self) -> dict:
+        return {
+            "Authorization": f"Bearer {self._get_access_token()}",
+            "Content-Type": "application/json"
+        }
+    
+    def _get_access_token(self) -> str:
+        if _token_cache["token"] and _token_cache["expires_at"] > time.time():
+            print("Token in cache still active.")
+            return _token_cache["token"]
+        
+        print("Token in cache is inactive. Refreshing Token.")
+        return self._refresh_token()
+            
+    def _refresh_token(self) -> str:
+        try:
+            response = requests.post(
+                self.BASE_AUTH_URL,
+                data={
+                    "grant_type": "refresh_token",
+                    "refresh_token": self.REFRESH_TOKEN,
+                },
+                headers={
+                    "content-type": "application/x-www-form-urlencoded",
+                    "Authorization": "Basic " + base64.b64encode(self.CLIENT_CREDENTIALS).decode()
+                }
+            )
+            response.raise_for_status()
+            data = response.json()
+            # around here add handling of invalid case ---> "invalid_grant" returned
+            # (https://developer.spotify.com/documentation/web-api/tutorials/refreshing-tokens#:~:text=encoded%20client_id%3Aclient_secret%3E-,Example,-The%20following%20code)
+
+            self._save_token_details(data)
+
+            return data.get("access_token")
+        except requests.exceptions.RequestException as e:
+            raise Exception(f"Failed to refresh access token: {str(e)}")
+    
+    def _save_token_details(self, data: dict) -> None:
+        _token_cache["token"] = data.get("access_token")
+        _token_cache["expires_at"] = time.time() + data.get("expires_in") - 60
+
+
+@dataclass
+class Song:
+    uri: str
+    name: str
+    artists: list[str]
+    year: int
+
+    def __str__(self) -> str:
+        return f"""
+        URI: {self.uri}
+        SONG NAME: {self.name}
+        ARTISTS: {", ".join([artist for artist in self.artists])}
+        YEAR: {self.year}
+        """
+
+def process_playlist_items(items: list[dict]) -> list[Song]:
+    return [
+        Song(
+            uri=item["item"]["uri"],
+            name=item["item"]["name"],
+            artists=[artist["name"] for artist in item["item"]["artists"]],
+            year=int(get_year_from_release_date(item["item"]["album"]["release_date"]))
+        ) 
+        for item in items
+    ]
+
+def get_year_from_release_date(date: str):
+    return date.split('-')[0]
+
+if __name__ == "__main__":
+    client = SpotifyClient()
+    playlist_id = "5GsuH4JNT7uiPGSKArlteE"
+    
+    playlist_items = client.fetch_playlist_items(playlist_id)
+    songs = process_playlist_items(playlist_items["items"])
+
+    for song in songs:
+        print(song)
+    
+    print(f"Number of songs collected: {len(playlist_items["items"])}")
