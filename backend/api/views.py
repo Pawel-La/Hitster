1c26db80f523bee2d392e5ba11a06ea580451f89
Pawel Lamza <pawel.lamza@swmansion.com>
2026-07-25 19:34:49 +0200

    small styling

--- backend/api/views.py
+++ backend/api/views.py
@@ -19,21 +19,24 @@ class PlaylistView(APIView):
     def get(self, request, playlist_id=None):
         if playlist_id is None:
             playlist_id = request.query_params.get("playlist_id", DEFAULT_PLAYLIST_ID)
+
         try:
             client = SpotifyClient()
-            playlist_items = client.fetch_playlist_items(playlist_id)
-            songs = process_playlist_items(playlist_items)
-            random.shuffle(songs)
         except ValueError as e:
             return Response(
                 {"detail": str(e)},
                 status=status.HTTP_500_INTERNAL_SERVER_ERROR,
             )
+
+        try:
+            playlist_items = client.fetch_playlist_items(playlist_id)
         except SpotifyClientException as e:
             return Response(
                 {"detail": str(e)},
                 status=status.HTTP_502_BAD_GATEWAY,
             )
 
+        songs = process_playlist_items(playlist_items)
+        random.shuffle(songs)
         serializer = PlaylistSongSerializer(songs, many=True)
         return Response(serializer.data)
